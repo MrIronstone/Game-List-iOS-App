6d0c998795bc3e39bbfd749580c39691f079d806
Hüsamettin Demirtaş <husamdemirtas1@gmail.com>
2022-06-29 20:12:40 +0300

    Initial Commit

---
+++ GameListAppWithUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameListAppWithUIKit
+//
+//  Created by Hüsamettin Demirtaş on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
